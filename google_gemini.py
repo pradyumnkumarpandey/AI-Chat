5a2397a01a9a809df26944d97fd937ceff2aa0d1
pradyumnkumarpandey <72699437+pradyumnkumarpandey@users.noreply.github.com>
2025-01-10 12:52:23 +0530

    Update google_gemini.py

--- google_gemini.py
+++ google_gemini.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from decouple import config
+import logging
+logger = logging.getLogger(__name__)
 def get_gemini_reponse(message):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key={config('API_KEY')}"
 
@@ -20,8 +22,10 @@ def get_gemini_reponse(message):
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
+    logger.info(json.dumps({"status":"response from AI","status":str(response)}))
     data = response.text
     data = json.loads(data)
+    logger.info({"status":"response from AI- data","data":data})
     print(type(data))
     return data['candidates'][0]['content']['parts'][0]['text']
     # print(response.text)
